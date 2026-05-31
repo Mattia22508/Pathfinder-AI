2211cec7df827f142c5de653d18bb9dfec00b531
Mattia_La_Barbera <labarbera2223@giorgimi.edu.it>
2026-05-31 15:50:41 +0200

    1110

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ oauth = OAuth(app)
 google = oauth.register(
     name='google',
     client_id='413339596512-evho3j40c9iss0uoka9me5656c6tlpfv.apps.googleusercontent.com', 
+    client_secret='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',
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'}
 )
